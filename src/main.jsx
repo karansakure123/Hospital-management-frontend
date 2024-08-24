4c4d1afbc0dae9f4833b372ffb17ac977d7dca6e
Laxman Sakure <laxmansakure007@gmail.com>
2024-08-24 15:26:05 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
@@ -6,12 +6,17 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import './index.css';
 
+export const Context = createContext({});
+
 const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+
   return (
-    <>
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
       <App />
       <ToastContainer position="top-center" autoClose={3000} />
-    </>
+    </Context.Provider>
   );
 };
 
@@ -20,4 +25,3 @@ createRoot(document.getElementById('root')).render(
     <AppWrapper />
   </React.StrictMode>
 );
- 
