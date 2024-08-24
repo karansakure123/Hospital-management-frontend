cde535e45f9151b5c4597ec1d64660544248d391
Laxman Sakure <laxmansakure007@gmail.com>
2024-08-24 09:32:50 +0530

    auth  changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,18 +27,37 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Home</Link>
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/appointment">Appointment</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/department">Departments</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/doctors">Doctors</Link>
+            </li>
             {isAuthenticated ? (
-              <>
-                <li className="nav-item">
-                  <button className="nav-link btn" onClick={handleLogout}>Logout</button>
-                </li>
-              </>
+              <li className="nav-item">
+                <button className="nav-link btn" onClick={handleLogout}>Logout</button>
+              </li>
             ) : (
               <>
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Register</Link>
                 </li>
               </>
             )}
