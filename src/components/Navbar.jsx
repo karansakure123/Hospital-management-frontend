69454a35372cf49d9a33b58436122797e3bb893a
Laxman Sakure <laxmansakure007@gmail.com>
2024-08-24 09:37:59 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,11 +25,15 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Home</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+     <div className="container-fluid justify-content-between">
+                <Link className="navbar-brand" to="/">
+                    <img src="https://renovahospitals.com/images/Renova-Logo.png" alt="Hospital Logo" className='nav-logo' />
+                </Link>
+                <button className="navbar-toggler" type="button" onClick={toggleSidebar}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
